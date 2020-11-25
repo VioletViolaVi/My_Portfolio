35365cd885253ab810478521d530d06f9d10e4d7
VioletViolaVi <vivian.v18723@gmail.com>
2020-11-25 16:14:21 +0000

    Add functionality for back to top button.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,19 @@
 $("document").ready(function () {
-    
+	// scrolls back to top
+	$(".fas.fa-arrow-up").click(function () {
+		window.scrollTo({
+			top: 0,
+		});
+	});
+
+	// shows and hides back to top button
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 300) {
+			$(".back-to-top-btn").show();
+		} else {
+			$(".back-to-top-btn").hide();
+		}
+	});
     
 
 
