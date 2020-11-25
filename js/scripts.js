4f063bf2d4123d68c6a2aecaaaf78ea2c11a380b
VioletViolaVi <vivian.v18723@gmail.com>
2020-11-25 12:01:44 +0000

    Restart portfolio by rearranging directories and external links and removing previous code.

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-// waits until DOM has been fully loaded
-$("document").ready(function () {
-  // slides navbar in/out
-  $(".icon.fas.fa-bars").click(function () {
-    $("#navContainer").toggleClass("active");     
-  });
-});
