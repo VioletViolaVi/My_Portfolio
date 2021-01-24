83ff6db8dc6dbfdf4887b6ac41195b72ec56381c
VioletViolaVi <vivian.v18723@gmail.com>
2021-01-24 21:12:18 +0000

    Remove all parts of portfolio.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,17 +1,3 @@
 $("document").ready(function () {
-	// scrolls back to top
-	$(".fas.fa-arrow-up").click(function () {
-		window.scrollTo({
-			top: 0,
-		});
-	});
-
-	// shows and hides back to top button
-	$(window).scroll(function () {
-		if ($(this).scrollTop() > 300) {
-			$(".back-to-top-btn").show();
-		} else {
-			$(".back-to-top-btn").hide();
-		}
-	});
+	console.log("hello world");
 });
