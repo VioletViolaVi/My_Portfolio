13347cb391e43c1f8c5f40a159690c9e0b92d7ba
VioletViolaVi <vivian.v18723@gmail.com>
2021-01-25 15:38:02 +0000

    Add functionality to back to top button.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,17 @@
 $("document").ready(function () {
-	console.log("hello world");
+  // scrolls to screen top using btn
+  $(".far.fa-caret-square-up").click(function () {
+    window.scrollTo({
+      top: 0,
+    });
+  });
+
+  // shows & hides back to top btn
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $("#backToTop").show();
+    } else {
+      $("#backToTop").hide();
+    }
+  });
 });
