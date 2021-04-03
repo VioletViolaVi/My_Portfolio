78f391e45de76bb5dc9c07f8946ee6bbabe83a6b
VioletViola <vivian.v18723@gmail.com>
2021-04-03 06:28:58 +0100

    Style home portion of portfolio.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,22 @@
 $("document").ready(function () {
+    // toggles nav links
     $("#header .fas.fa-bars").on("click", () => {
         $(".nav-links").fadeToggle();
+    });
+
+    // goes back to top
+    $("#backToTopIcon").on("click", () => {
+        window.scrollTo({
+            top: 0,
+        });
+    });
+
+    // toggles back to top btn
+    $(window).on("scroll", () => {
+        if ($(this).scrollTop() > 300) {
+            $("#backToTopAside").show();
+        } else {
+            $("#backToTopAside").hide();
+        }
     });
 });
